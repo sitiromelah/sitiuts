5a336287a8243e203ee66048c5c1a8e7a0d3bd65
sitiromelah <sitiromelah47@gmail.com>
2020-04-11 20:45:26 +0700

    Main dan manifest

--- app/src/main/java/com/siti/sitiuts/MainActivity.java
+++ app/src/main/java/com/siti/sitiuts/MainActivity.java
@@ -2,13 +2,27 @@ package com.siti.sitiuts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonMasuk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonMasuk = findViewById(R.id.buttonMasuk);
+
+        buttonMasuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
